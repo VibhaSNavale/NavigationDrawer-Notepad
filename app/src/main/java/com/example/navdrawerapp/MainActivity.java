f62d86c73b3d2ca68a04aa9b71da0007d1003239
VibhaSN <vibha.navale11@gmail.com>
2020-05-26 18:05:48 +0530

    Editing and viewing profile, and saving note features

--- app/src/main/java/com/example/navdrawerapp/MainActivity.java
+++ app/src/main/java/com/example/navdrawerapp/MainActivity.java
@@ -32,13 +32,18 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_profile, R.id.nav_editprofile, R.id.nav_notes)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+//        final ParseUser parseUser = new ParseUser();
+//        String userName = parseUser.getUsername();
+//
+//        TextView txtProfileName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.user);
+//        txtProfileName.setText(userName);
     }
 
     @Override
